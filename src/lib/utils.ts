5b9eb4eea12129d686fb87cd7f7fdf0f81474c46
beruf <rube@ruf2.de>
2024-07-24 17:54:01 +0200

    feat: new utils for zchfaddress per chain

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,16 @@ export function blockExplorerLink(chainId: number | undefined, hash: string) {
       return `https://etherscan.io/tx/${hash}`;
   }
 }
+
+export function getZCHFAddress(chainId: number | undefined) {
+  switch (chainId) {
+    case 1:
+      return '0xB58E61C3098d85632Df34EecfB899A1Ed80921cB';
+    case 137:
+      return '0x02567e4b14b25549331fCEe2B56c647A8bAB16FD';
+    case 11155111:
+      return '0xB58E61C3098d85632Df34EecfB899A1Ed80921cB';
+    default:
+      return '0xB58E61C3098d85632Df34EecfB899A1Ed80921cB';
+  }
+}
