d74c0a4aaa76f595aac357b464412b6cd4045059
beruf <rube@ruf2.de>
2024-07-24 17:53:34 +0200

    feat: add sepolia network

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -16,22 +16,32 @@ export default defineConfig({
           name: 'FactoryManager',
           address: {
             [mainnet.id]: '0x555E7852d4ab6F8C557F9Bc6d17ADdb8c7911d78',
-          },
-        },
-      ],
-    }),
-    etherscan({
-      apiKey: process.env.POLYGONSCAN_API_KEY!,
-      chainId: polygon.id,
-      contracts: [
-        {
-          name: 'FactoryManager',
-          address: {
+            [sepolia.id]: '0x555E7852d4ab6F8C557F9Bc6d17ADdb8c7911d78',
             [polygon.id]: '0x555E7852d4ab6F8C557F9Bc6d17ADdb8c7911d78',
           },
         },
+        {
+          name: 'BrokerbotFactory',
+          address: {
+            [mainnet.id]: '0xfAe70dEBb64a7176aaA41D1d7fEAfc4CCA4a5107',
+            [sepolia.id]: '0xfAe70dEBb64a7176aaA41D1d7fEAfc4CCA4a5107',
+            [polygon.id]: '0xfAe70dEBb64a7176aaA41D1d7fEAfc4CCA4a5107',
+          },
+        }
       ],
     }),
+    // etherscan({
+    //   apiKey: process.env.POLYGONSCAN_API_KEY!,
+    //   chainId: polygon.id,
+    //   contracts: [
+    //     {
+    //       name: 'FactoryManager',
+    //       address: {
+    //         [polygon.id]: '0x555E7852d4ab6F8C557F9Bc6d17ADdb8c7911d78',
+    //       },
+    //     },
+    //   ],
+    // }),
     react(),
   ],
 })
